c5e8c14f6efbf316e0c84a47362930f56d40a84b
Danny Gershman <danny.gershman@gmail.com>
2018-09-24 01:41:15 -0400

    further template simplification

--- partials/views/_cities.php
+++ partials/views/_cities.php
@@ -6,48 +6,7 @@
 				<tr class="meeting-header">
 					<td colspan="3">{{city}}</td>
 				</tr>
-				{{#each meetings}}
-				<tr>
-					<td class="bmlt-column1">
-						<div class="bmlt-day">{{this.formatted_day}}</div>
-						<div class="bmlt-time-2">{{this.start_time_formatted}} - {{this.end_time_formatted}}</div>
-						<a id="bmlt-formats"
-						   class="btn btn-primary btn-xs"
-						   title=""
-						   data-html="true"
-						   tabindex="0"
-						   data-trigger="focus"
-						   role="button"
-						   data-toggle="popover"
-						   data-original-title=""
-						   data-content="
-								<table class='bmlt_a_format table-bordered'>
-								{{#each this.formats_expanded}}
-								<tr>
-									<td class='formats_key'>{{key}}</td>
-									<td class='formats_name'>{{name}}</td>
-									<td class='formats_description'>{{description}}</td>
-								</tr>
-								{{/each}}
-							</table>">
-						<span class="glyphicon glyphicon-search" aria-hidden="true"></span>{{ this.formats }}
-						</a>
-						<div class="bmlt-comments">{{this.formatted_comments}}</div>
-					</td>
-					<td class="bmlt-column2">
-						<div class="meeting-name">{{this.meeting_name}}</div>
-						<div class="location-text">{{this.location_text}}</div>
-						<div class="meeting-address">{{this.formatted_address}}</div>
-						<div class="location-information">{{this.formatted_location_inf}}</div>
-					</td>
-					<td class="bmlt-column3">
-						<a target="_blank" href="https://maps.google.com/maps?q={{this.latitude}},{{this.longitude}}" id="map-button" class="btn btn-primary btn-xs">
-							<span class="glyphicon glyphicon-map-marker"></span> MAP
-						</a>
-						<div class='geo hide'>{{this.latitude}},{{this.longitude}}</div>
-					</td>
-				</tr>
-				{{/each}}
+				<?php include '_meetings.php'; ?>
 			{{/each}}
 			</tbody>
 		</table>
@@ -70,5 +29,6 @@
 		}
 		var html = template(context);
 		jQuery("#cities").append(html);
+		jQuery(".bmlt-day").removeClass("hide");
 	});
 </script>
--- partials/views/_weekdays.php
+++ partials/views/_weekdays.php
@@ -5,47 +5,7 @@
 		<div id="bmlt-table-div">
 			<table class='bmlt-table table table-striped table-hover table-bordered tablesaw tablesaw-stack'>
 				<tbody>
-					{{#each meetings}}
-					<tr>
-						<td class="bmlt-column1">
-							<div class="bmlt-time-2">{{this.start_time_formatted}} - {{this.end_time_formatted}}</div>
-							<a id="bmlt-formats"
-							   class="btn btn-primary btn-xs"
-							   title=""
-							   data-html="true"
-							   tabindex="0"
-							   data-trigger="focus"
-							   role="button"
-							   data-toggle="popover"
-							   data-original-title=""
-							   data-content="
-									<table class='bmlt_a_format table-bordered'>
-									{{#each this.formats_expanded}}
-									<tr>
-										<td class='formats_key'>{{key}}</td>
-										<td class='formats_name'>{{name}}</td>
-										<td class='formats_description'>{{description}}</td>
-									</tr>
-									{{/each}}
-								</table>">
-								<span class="glyphicon glyphicon-search" aria-hidden="true"></span>{{ this.formats }}
-							</a>
-							<div class="bmlt-comments">{{this.formatted_comments}}</div>
-						</td>
-						<td class="bmlt-column2">
-							<div class="meeting-name">{{this.meeting_name}}</div>
-							<div class="location-text">{{this.location_text}}</div>
-							<div class="meeting-address">{{this.formatted_address}}</div>
-							<div class="location-information">{{this.formatted_location_inf}}</div>
-						</td>
-						<td class="bmlt-column3">
-							<a target="_blank" href="https://maps.google.com/maps?q={{this.latitude}},{{this.longitude}}" id="map-button" class="btn btn-primary btn-xs">
-								<span class="glyphicon glyphicon-map-marker"></span> MAP
-							</a>
-							<div class='geo hide'>{{this.latitude}},{{this.longitude}}</div>
-						</td>
-					</tr>
-					{{/each}}
+					<?php include '_meetings.php'; ?>
 				</tbody>
 			</table>
 		</div>
