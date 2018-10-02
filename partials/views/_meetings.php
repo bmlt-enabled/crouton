42d396f5694fc2c54f66ede034a0a1d3682dc064
Danny Gershman <danny.gershman@gmail.com>
2018-10-01 20:10:44 -0400

    area dropdown working now

--- partials/views/_meetings.php
+++ partials/views/_meetings.php
@@ -4,7 +4,8 @@
 	data-groups="{{formatDataPointer this.meeting_name}}"
 	data-locations="{{formatDataPointer this.location_text}}"
 	data-zips="{{formatDataPointer this.location_postal_code_1}}"
-	data-formats="{{formatDataPointerFormats this.formats_expanded}}">
+	data-formats="{{formatDataPointerFormats this.formats_expanded}}"
+	data-areas="{{formatDataPointer this.service_body_bigint}}">
 	<td class="bmlt-column1">
 		<div class="bmlt-day">{{this.formatted_day}}</div>
 		<div class="bmlt-time-2">{{this.start_time_formatted}} - {{this.end_time_formatted}}</div>
