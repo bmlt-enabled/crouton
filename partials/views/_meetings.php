18c2c7bd4d5a40a9d5f5608624205f34c2e3f512
Danny Gershman <danny.gershman@gmail.com>
2018-10-09 22:24:53 -0400

    Don't show the format icon if there are no formats #41

--- partials/views/_meetings.php
+++ partials/views/_meetings.php
@@ -10,27 +10,29 @@
 	<td class="bmlt-column1">
 		<div class="bmlt-day">{{this.formatted_day}}</div>
 		<div class="bmlt-time-2">{{this.start_time_formatted}} - {{this.end_time_formatted}}</div>
-		<a id="bmlt-formats"
-		   class="btn btn-primary btn-xs"
-		   title=""
-		   data-html="true"
-		   tabindex="0"
-		   data-trigger="focus"
-		   role="button"
-		   data-toggle="popover"
-		   data-original-title=""
-		   data-content="
-				<table class='bmlt_a_format table-bordered'>
-				{{#each this.formats_expanded}}
-				<tr>
-					<td class='formats_key'>{{key}}</td>
-					<td class='formats_name'>{{name}}</td>
-					<td class='formats_description'>{{description}}</td>
-				</tr>
-				{{/each}}
-			</table>">
-			<span class="glyphicon glyphicon-search" aria-hidden="true"></span>{{ this.formats }}
-		</a>
+		{{#if this.formats}}
+			<a id="bmlt-formats"
+			   class="btn btn-primary btn-xs"
+			   title=""
+			   data-html="true"
+			   tabindex="0"
+			   data-trigger="focus"
+			   role="button"
+			   data-toggle="popover"
+			   data-original-title=""
+			   data-content="
+					<table class='bmlt_a_format table-bordered'>
+					{{#each this.formats_expanded}}
+					<tr>
+						<td class='formats_key'>{{key}}</td>
+						<td class='formats_name'>{{name}}</td>
+						<td class='formats_description'>{{description}}</td>
+					</tr>
+					{{/each}}
+				</table>">
+				<span class="glyphicon glyphicon-search" aria-hidden="true"></span>{{ this.formats }}
+			</a>
+		{{/if}}
 		<div class="bmlt-comments">{{this.formatted_comments}}</div>
 	</td>
 	<td class="bmlt-column2">
