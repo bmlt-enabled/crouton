d46606294024a6897485d3fdf8b08077ae4d3d38
Paul Nagle <paulnagle@gmail.com>
2019-03-18 16:55:27 +0000

    Danish translation

---
+++ lang/da-DK.php
@@ -0,0 +1,15 @@
+<?php
+static $words = array(
+    'days_of_the_week' => [1 => "Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"],
+    'weekday' => 'Ugedag',
+    'city' => 'By',
+    'cities' => 'Byer',
+    'groups' => 'Grupper',
+    'areas' => 'Områder',
+    'locations' => 'Lokalitet',
+    'counties' => 'Amter',
+    'states' => 'Provinser',
+    'postal_codes' => 'Post nummer',
+    'formats' => 'Struktur',
+    'map' => 'Kort'
+);
