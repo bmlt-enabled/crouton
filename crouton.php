ed440d6e130b191928303092e59cf108385290a8
Danny Gershman <danny.gershman@gmail.com>
2019-09-29 12:04:29 -0400

    adding kevin theme #155

--- crouton.php
+++ crouton.php
@@ -23,6 +23,7 @@ if (!class_exists("Crouton")) {
         public static $HOUR_IN_SECONDS = 3600;
         public $themes = [
             "jack",
+            "kevin",
             "lucy",
             "patrick"
         ];
