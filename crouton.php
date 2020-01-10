7cdf087c6fa63eaa10923c7ac9ca1c1f62b1cb8d
Danny <danny.gershman@gmail.com>
2020-01-09 21:24:59 -0500

    changing shortcode + error handlings

--- crouton.php
+++ crouton.php
@@ -93,7 +93,7 @@ if (!class_exists("Crouton")) {
                     &$this,
                     "tabbedUi"
                 ));
-                add_shortcode('crouton_map', array(
+                add_shortcode('bmlt_tabs_map', array(
                     &$this,
                     "croutonMap"
                 ));
@@ -125,7 +125,7 @@ if (!class_exists("Crouton")) {
                 echo '<div class="bootstrap-bmlt" id="please-wait"><button class="btn btn-lg btn-info"><span class="glyphicon glyphicon-repeat glyphicon-repeat-animate"></span>Fetching...</button></div>';
                 return true;
             }
-            if (stripos($post_to_check->post_content, '[crouton_map') !== false) {
+            if (stripos($post_to_check->post_content, '[bmlt_tabs_map') !== false) {
                 return true;
             }
             if (stripos($post_to_check->post_content, '[bmlt_count') !== false) {
@@ -327,6 +327,7 @@ if (!class_exists("Crouton")) {
 
         public function renderMap($atts)
         {
+            if ($atts == null) $atts = array();
             $atts['map_search'] = (object)[
                 "zoom" => 10,
                 "latitude" => 0,
