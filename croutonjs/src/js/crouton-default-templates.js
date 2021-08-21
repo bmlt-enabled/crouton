909dd9e4586b28702e232079e532f7902e78bbaf
Alan B <918773+alanb2718@users.noreply.github.com>
2021-08-21 16:30:15 -0700

    format templates to make them easier to modify and extend

--- croutonjs/src/js/crouton-default-templates.js
+++ croutonjs/src/js/crouton-default-templates.js
@@ -1,5 +1,51 @@
 var croutonDefaultTemplates = {
-	meeting_data_template: "{{#isTemporarilyClosed this}}<div class='temporarilyClosed'><span class='glyphicon glyphicon-flag'></span> {{temporarilyClosed this}}</div>{{/isTemporarilyClosed}}<div class='meeting-name'>{{this.meeting_name}}</div><div class='location-text'>{{this.location_text}}</div><div class='meeting-address'>{{this.formatted_address}}</div><div class='location-information'>{{this.formatted_location_info}}</div>{{#if this.virtual_meeting_additional_info}}<div class='meeting-additional-info'>{{this.virtual_meeting_additional_info}}</div>{{/if}}",
-	metadata_template: "{{#isVirtual this}}{{#isHybrid this}}<div class='meetsVirtually'><span class='glyphicon glyphicon-cloud-upload'></span> {{meetsHybrid this}}</div>{{else}}<div class='meetsVirtually'><span class='glyphicon glyphicon-cloud'></span> {{meetsVirtually this}}</div>{{/isHybrid}}{{#if this.virtual_meeting_link}}<div><span class='glyphicon glyphicon-globe'></span> {{webLinkify this.virtual_meeting_link}}</div>{{#if this.show_qrcode}}<div class='qrcode'>{{qrCode this.virtual_meeting_link}}</div>{{/if}}{{/if}}{{#if this.phone_meeting_number}}<div><span class='glyphicon glyphicon-earphone'></span> {{phoneLinkify this.phone_meeting_number}}</div>{{#if this.show_qrcode}}<div class='qrcode'>{{qrCode this.phone_meeting_number}}</div>{{/if}}{{/if}}{{/isVirtual}}{{#isNotTemporarilyClosed this}}{{#unless (hasFormats 'VM' this)}}<div><a id='map-button' class='btn btn-primary btn-xs' href='https://www.google.com/maps/search/?api=1&query={{this.latitude}},{{this.longitude}}&q={{this.latitude}},{{this.longitude}}' target='_blank' rel='noopener noreferrer'><span class='glyphicon glyphicon-map-marker'></span> {{this.map_word}}</a></div><div class='geo hide'>{{this.latitude}},{{this.longitude}}</div>{{/unless}}{{/isNotTemporarilyClosed}}",
-	observer_template: "<div class='observerLine'>{{this.contact_name_1}} {{this.contact_phone_1}} {{this.contact_email_1}}</div><div class='observerLine'>{{this.contact_name_2}} {{this.contact_phone_2}} {{this.contact_email_2}}</div>"
+
+meeting_data_template:
+`{{#isTemporarilyClosed this}}
+    <div class='temporarilyClosed'><span class='glyphicon glyphicon-flag'></span> {{temporarilyClosed this}}</div>
+{{/isTemporarilyClosed}}
+<div class='meeting-name'>{{this.meeting_name}}</div>
+<div class='location-text'>{{this.location_text}}</div>
+<div class='meeting-address'>{{this.formatted_address}}</div>
+<div class='location-information'>{{this.formatted_location_info}}</div>
+{{#if this.virtual_meeting_additional_info}}
+    <div class='meeting-additional-info'>{{this.virtual_meeting_additional_info}}</div>
+{{/if}}`,
+
+metadata_template:
+`{{#isVirtualOrHybrid this}}
+    {{#isHybrid this}}
+        <div class='meetsVirtually'><span class='glyphicon glyphicon-cloud-upload'></span> {{meetsHybrid this}}</div>
+    {{else}}
+        <div class='meetsVirtually'><span class='glyphicon glyphicon-cloud'></span> {{meetsVirtually this}}</div>
+    {{/isHybrid}}
+    {{#if this.virtual_meeting_link}}
+        <div><span class='glyphicon glyphicon-globe'></span> {{webLinkify this.virtual_meeting_link}}</div>
+        {{#if this.show_qrcode}}
+            <div class='qrcode'>{{qrCode this.virtual_meeting_link}}</div>
+        {{/if}}
+    {{/if}}
+    {{#if this.phone_meeting_number}}
+        <div><span class='glyphicon glyphicon-earphone'></span> {{phoneLinkify this.phone_meeting_number}}</div>
+        {{#if this.show_qrcode}}
+            <div class='qrcode'>{{qrCode this.phone_meeting_number}}</div>
+        {{/if}}
+    {{/if}}
+{{/isVirtualOrHybrid}}
+{{#isNotTemporarilyClosed this}}
+    {{#unless (hasFormats 'VM' this)}}
+        <div>
+            <a id='map-button' class='btn btn-primary btn-xs'
+                href='https://www.google.com/maps/search/?api=1&query={{this.latitude}},{{this.longitude}}&q={{this.latitude}},{{this.longitude}}'
+                target='_blank' rel='noopener noreferrer'>
+                <span class='glyphicon glyphicon-map-marker'></span> {{this.map_word}}</a>
+        </div>
+        <div class='geo hide'>{{this.latitude}},{{this.longitude}}</div>
+    {{/unless}}
+{{/isNotTemporarilyClosed}}`,
+
+observer_template:
+`<div class='observerLine'>{{this.contact_name_1}} {{this.contact_phone_1}} {{this.contact_email_1}}</div>
+<div class='observerLine'>{{this.contact_name_2}} {{this.contact_phone_2}} {{this.contact_email_2}}</div>`
+
 }
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: na, meeting list, meeting finder, maps, recovery, addiction, webservant, b
 Requires at least: 4.0
 Required PHP: 5.6
 Tested up to: 5.8.0
-Stable tag: 3.12.3
+Stable tag: 3.12.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 crouton implements a Tabbed UI for BMLT.
@@ -35,6 +35,9 @@ Crouton was forked from BMLT Tabbed UI plugin in 2018.  This plugin provides a T
 https://demo.bmlt.app/crouton
 
 == Changelog ==
+
+= 3.12.4 =
+* Format Meeting Data Template, Metadata Template, and Observer Template to make them easier to modify and extend
 
 = 3.12.3 =
 * Updates for pt-BR translations.
