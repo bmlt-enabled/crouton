63f27a026b4247731710a6ac0afbabcc32c0ec9b
otrok7 <50595291+otrok7@users.noreply.github.com>
2024-04-08 16:55:40 +0200

    move QR code service

--- croutonjs/src/js/crouton-core.js
+++ croutonjs/src/js/crouton-core.js
@@ -1535,7 +1535,7 @@ crouton_Handlebars.registerHelper('meetsHybrid', function(data, options) {
 });
 
 crouton_Handlebars.registerHelper('qrCode', function(link, options) {
-	return new crouton_Handlebars.SafeString("<img alt='qrcode' src='https://chart.googleapis.com/chart?chs=100x100&cht=qr&chl=" + link + "&choe=UTF-8&chld=L|0'>");
+	return "<img alt='qrcode' src='https://quickchart.io/qr?size=100&text=" + encodeURIComponent(link) + "'>";
 });
 
 crouton_Handlebars.registerHelper('formatDataFromArray', function(arr) {
