08f245c93651429ce487a86ba6cdc4ea789d8329
otrok7 <50595291+otrok7@users.noreply.github.com>
2025-11-07 11:22:07 +0100

    Remove unneccessary bloat: Tablesaw.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,6 @@ let jsFilesCrouton = [
 	'bootstrap.min.js',
 	'transition.js',
 	'select2.full.min.js',
-	'tablesaw.jquery.3.0.9.js',
 	'handlebars-v4.5.3.js',
 	'moment.js',
 	'moment-timezone.js',
