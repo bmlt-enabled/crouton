40a4b7af19c53f79bd3a457982bfcfcb16e86049
otrok7 <50595291+otrok7@users.noreply.github.com>
2026-08-30 15:37:01 +0200

    Adding autocomplete with cities

--- croutonjs/src/js/meeting_map.js
+++ croutonjs/src/js/meeting_map.js
@@ -19,6 +19,7 @@ function MeetingMap(inConfig) {
 	var gOldBounds = false;
 	var gCurrentlyDraggingMap = false;
 	var gLocationSearchResult = null;
+	var gAllCities = [];
 	function preloadApiLoadedCallback(f,a) {
 		gLoadedCallbackFunction = f;
 		gLoadedCallbackArgs = a;
@@ -175,12 +176,99 @@ function MeetingMap(inConfig) {
 		controlDiv.querySelector("#location-search-button").addEventListener('click', showLocationSearchDialog);
 		gDefaultLocationSearchButtonLabel = controlDiv.querySelector("#location-search-button :first-child").innerHTML;
 		controlDiv.querySelector("#bmlt-location-search-nearbyMeetings").addEventListener('click', doGeolocationThenClose);
-		controlDiv.querySelector("#bmlt-location-search-goto-text").addEventListener('keypress', function (event) {
-			if (event.key === "Enter") {
-				event.preventDefault();
-				document.getElementById("bmlt-location-search-text-button").click();
+		var currentFocus;
+  		controlDiv.querySelector("#bmlt-location-search-goto-text").addEventListener("input", function(e) {
+      		var a, b, i, val = this.value;
+			const inp = this;
+      		/*close any already open lists of autocompleted values*/
+      		closeAllLists();
+      		if (!val) { return false;}
+      		currentFocus = -1;
+      		/*create a DIV element that will contain the items (values):*/
+      		a = document.createElement("DIV");
+      		a.setAttribute("id", this.id + "autocomplete-list");
+      		a.setAttribute("class", "autocomplete-items");
+      		/*append the DIV element as a child of the autocomplete container:*/
+      		this.parentNode.appendChild(a);
+      		/*for each item in the array...*/
+      		for (i = 0; i < gAllCities.length; i++) {
+        		/*check if the item starts with the same letters as the text field value:*/
+        		if (gAllCities[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          			/*create a DIV element for each matching element:*/
+          			b = document.createElement("DIV");
+	          		/*make the matching letters bold:*/
+    	      		b.innerHTML = "<strong>" + gAllCities[i].substr(0, val.length) + "</strong>";
+        	  		b.innerHTML += gAllCities[i].substr(val.length);
+          			/*insert a input field that will hold the current array item's value:*/
+          			b.innerHTML += "<input type='hidden' value='" + gAllCities[i] + "'>";
+	          		/*execute a function when someone clicks on the item value (DIV element):*/
+    	      		b.addEventListener("click", function(event) {
+        	      		/*insert the value for the autocomplete text field:*/
+            	  		inp.value = this.getElementsByTagName("input")[0].value;
+              			/*close the list of autocompleted values,
+              			(or any other open lists of autocompleted values:*/
+              			closeAllLists();
+						document.getElementById("bmlt-location-search-text-button").click();
+          			});
+          			a.appendChild(b);
+        		}
 			}
-		});
+  		});
+  		/*execute a function presses a key on the keyboard:*/
+  		controlDiv.querySelector("#bmlt-location-search-goto-text").addEventListener("keydown", function(e) {
+      		var x = document.getElementById(this.id + "autocomplete-list");
+			console.log(x);
+      		if (x) x = x.getElementsByTagName("div");
+      		if (e.keyCode == 40) {
+        		/*If the arrow DOWN key is pressed,
+        		increase the currentFocus variable:*/
+        		currentFocus++;
+        		/*and and make the current item more visible:*/
+        		addActive(x);
+      		} else if (e.keyCode == 38) { //up
+        		/*If the arrow UP key is pressed,
+        		decrease the currentFocus variable:*/
+        		currentFocus--;
+        		/*and and make the current item more visible:*/
+        		addActive(x);
+      		} else if (e.keyCode == 13) {
+        		/*If the ENTER key is pressed, prevent the form from being submitted,*/
+        		e.preventDefault();
+        		if (currentFocus > -1) {
+          			/*and simulate a click on the "active" item:*/
+          			if (x) x[currentFocus].click();
+        		}
+      		}
+  		});
+  		function addActive(x) {
+    		/*a function to classify an item as "active":*/
+    		if (!x) return false;
+    		/*start by removing the "active" class on all items:*/
+    		removeActive(x);
+    		if (currentFocus >= x.length) currentFocus = 0;
+    		if (currentFocus < 0) currentFocus = (x.length - 1);
+    		/*add class "autocomplete-active":*/
+    		x[currentFocus].classList.add("autocomplete-active");
+			console.log(x[currentFocus]);
+  		}
+  		function removeActive(x) {
+    		/*a function to remove the "active" class from all autocomplete items:*/
+    		for (var i = 0; i < x.length; i++) {
+      			x[i].classList.remove("autocomplete-active");
+    		}
+  		}
+  		function closeAllLists() {
+    		var x = document.getElementsByClassName("autocomplete-items");
+    		for (var i = 0; i < x.length; i++) {
+  		      	x[i].parentNode.removeChild(x[i]);
+    		}
+  		}
+  		/*execute a function when someone clicks in the document:*/
+			document.addEventListener("click", function (e) {
+      		closeAllLists(e.target);
+  		});
+
+
 		controlDiv.querySelector("#bmlt-location-search-text-button").addEventListener('click', function () {
 			let text = document.getElementById("bmlt-location-search-goto-text").value.trim();
 			if (text === "") return;
@@ -542,6 +630,7 @@ function MeetingMap(inConfig) {
 			return;
 		}
 		gAllMeetings = meetings_responseObject.filter(m => m.venue_type != 2);
+		gAllCities = [...new Set(meetings_responseObject.map(m => m['location_municipality']))].sort();
 		if (fitBounds && !config.centerMe && !config.goto) {
 			let lat_lngs = gAllMeetings.reduce(function(a,m) {a.push([m.latitude, m.longitude]); return a;},[]);
 			const maxRadius = config.maxTomatoWidth/2.0;
@@ -684,7 +773,8 @@ function MeetingMap(inConfig) {
 		}
 		jQuery('#bmlt_location_search_modal .crouton-modal-search-page').removeClass('hide');
 		jQuery('#bmlt_location_search_modal .crouton-modal-search-choice').addClass('hide');
-		jQuery('bmlt-location-search-goto-text').val('');
+		jQuery('#bmlt-location-search-goto-text').val('');
+		jQuery('#bmlt-location-search-goto-textautocomplete-list').remove();
 	}
 	function showGeocodingDialog(e=null) {
 		openModalWindow(document.getElementById('geocoding_modal'));
